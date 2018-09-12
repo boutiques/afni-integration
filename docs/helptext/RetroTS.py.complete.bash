e07ad7ecce764eb0097a19be7aa425bab0e18f25
Ross Markello <rossmarkello@gmail.com>
2018-09-12 10:41:45 -0400

    [FIX] Fix RetroTS.py help text

--- docs/helptext/RetroTS.py.complete.bash
+++ docs/helptext/RetroTS.py.complete.bash
@@ -1,1 +1,1 @@
-ARGS=() ; complete -W "${ARGS[*]}" -o bashdefault -o default RetroTS.py ; ##RetroTS.py##
+ARGS=('-slice_offset' '-v' ) ; complete -W "${ARGS[*]}" -o bashdefault -o default RetroTS.py ; ##RetroTS.py##
